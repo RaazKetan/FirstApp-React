a69bc9ca0ed4388abb49cd4fbccca040d4b1d93d
Raaz <21ketanraaz@gmail.com>
2023-12-28 19:52:36 +0530

    Update working with event handlers  commit in my-app

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,14 +3,20 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 //named props that are passed to the function key value pairs kesy are title, amount, date and value are the value set
 function ExpenseItem( props) {
-  
+
+    
+    let title = props.title;
+  const clickHandler = () => {
+    title = 'Updated!';
+  }
     return (
         <Card className="expense-item">
            <ExpenseDate date = {props.date}></ExpenseDate>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button onClick = {clickHandler}>Change Title</button>
         </Card>
     );
 }
